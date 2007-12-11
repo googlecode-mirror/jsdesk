4f15f653eb0f377d53ae55c7ec2b9c0507e5f04a
atomantic <atomantic@b4b69007-f53f-0410-8d90-83503aa1d848>
2007-12-11 06:07:33 +0000

    removed old code

--- trunk/js/Login.js
+++ trunk/js/Login.js
@@ -162,25 +162,6 @@ Login = function() {
                         method: 'POST',
                         success:Login.Success,
                         scope:Login
-                    //                            success: function (f, a) {
-                    //                                if (a && a.result) {
-                    //                                    win.destroy(true); // close dialog
-                    //                                    // set the cookie
-                    //                                    set_cookie('key', a.result.key, 30, '/', '', '');
-                    //                                    set_cookie('memberName', a.result.name, 30, '/', '', '');
-                    //                                    set_cookie('memberType', a.result.type, 30, '/', '', '');
-                    //                                    Ext.get('ext-gen15').hide(); // not sure why this mask from the waitMsg lingers :(
-                    //                                }
-                    //                            },
-                    //                            failure: function (f, a) {
-                    //                                var errorList = '';
-                    //                                for (var field in a.result.error) {
-                    //                                    var el = Ext.get(field);
-                    //                                    errorList += '<br />' + a.result.error[field];
-                    //                                    el.focus();
-                    //                                }
-                    //                                Ext.MessageBox.alert('Error', '' + errorList);
-                    //                            }
                     });
                 }
             });
@@ -223,60 +204,6 @@ Login = function() {
                         success:Login.Success,
                         scope:Login
                     });
-//                    var UserName = form.findField('UserName');
-                    //                    var Password = form.findField('Password');
-                    //                    var Password2 = Ext.get('Password2');
-                    //                    var Email = Ext.get('Email');
-                    //                    var FirstName = Ext.get('FirstName');
-                    //                    var LastName = Ext.get('LastName');
-                    //                    if (UserName.getValue() == '') {
-                    //                        Password.focus();
-                    //                        form.findField('UserName').focus(); // steal focus to activate validation
-                    //                        Password.focus();
-                    //                    } else if (Password.getValue() == '' || Password.getValue() != Password2.dom.value) {
-                    //                        Password2.focus();
-                    //                        form.findField('Password').focus();
-                    //                        Password2.focus();
-                    //                    } else if (Email.dom.value == Text.Lbl.EmptyEmail) {
-                    //                        Email.dom.value = '';
-                    //                        Email.focus();
-                    //                        form.findField('UserName').focus();
-                    //                        Email.focus();
-                    //                    } else if (FirstName.dom.value == '') {
-                    //                        FirstName.focus();
-                    //                        form.findField('UserName').focus();
-                    //                        FirstName.focus();
-                    //                    } else if (LastName.dom.value == '') {
-                    //                        LastName.focus();
-                    //                        form.findField('UserName').focus();
-                    //                        LastName.focus();
-                    //                    } else {
-                    //							var cryptpass = Ext.ux.Crypto.AES.encrypt(Password.getValue(), UserName.getValue(), 256); // encrypt using username as key
-                    //							Password.setValue(cryptpass);
-                    //							Password2.dom.value = cryptpass;
-
-                    //                                    success: function (f, a) {
-                    //                                        if (a && a.result) { // has some return info
-                    //                                            win.destroy(true); // close dialog
-                    //                                            Ext.get('ext-gen15').hide(); // not sure why this mask from the waitMsg lingers :(
-                    //                                            // set the cookie
-                    //                                            set_cookie('key', a.result.key, 30, '/', '', '' );
-                    //                                            set_cookie('memberName', a.result.name, 30, '/', '', '' );
-                    //                                            set_cookie('memberType', a.result.type, 30, '/', '', '' );
-                    //                                            // redirect the window
-                    //                                            //										window.location = "/desktop-demo/";
-                    //                                            //Ext.MessageBox.alert('Thanks', 'Your account has been created and are now logged in 8^]');
-                    //                                        }
-                    //                                    },
-                    //                                    failure: function (f, a) {
-                    //                                        var errorList = '';
-                    //                                        for (var field in a.result.error) {
-                    //                                            var el = Ext.get(field);
-                    //                                            errorList += '<br />' + a.result.error[field];
-                    //                                            el.focus();
-                    //                                        }
-                    //                                        Ext.MessageBox.alert('Error', '' + errorList);
-                    //                                    }
                 }
             });
             var btnCancel = new Ext.Button({
@@ -345,7 +272,21 @@ Login = function() {
                 set_cookie('memberUsername', a.result.username, '', '/', '', '');
 				// redirect the window
                 //window.location = path;
+                var y = this;
+               var x = Login;
+               var u = Login.app;
+                 var z = 3;
+
             }
+        },
+        failure: function (f, a) {
+            var errorList = '';
+            for (var field in a.result.error) {
+                var el = Ext.get(field);
+                errorList += '<br />' + a.result.error[field];
+                el.focus();
+            }
+            Ext.MessageBox.alert('Error', '' + errorList);
         }
     };
 }();
