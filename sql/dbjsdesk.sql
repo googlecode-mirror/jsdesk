4bc0fe22b4f3a433e3a9e30c92aa11e00ddc07b9
liuliming2008 <liuliming2008@b4b69007-f53f-0410-8d90-83503aa1d848>
2007-12-23 17:26:18 +0000

    fisheye done

--- sql/dbjsdesk.sql
+++ sql/dbjsdesk.sql
@@ -31,6 +31,9 @@ CREATE TABLE IF NOT EXISTS `desktop_config` (
 -- ----------------------------
 INSERT INTO `desktop_config` VALUES ('0', '[\"docs-win\"]','[\"docs-win\",\"grid-win\",\"tab-win\",\"bogus-menu\",\"acc-win\",\"layout-win\"]', '[\"docs-win\",\"grid-win\",\"tab-win\",\"acc-win\",\"layout-win\"]', '[\"preferences-win\"]', 'f9f9f9', 'Ladybuggin', 'center', 'Vista', 'false');
 INSERT INTO `desktop_config` VALUES ('3', '[\"docs-win\"]','[\"docs-win\",\"grid-win\",\"tab-win\",\"bogus-menu\",\"acc-win\",\"layout-win\"]', '[\"mytest-win\",\"docs-win\",\"layout-win\",\"grid-win\",\"tab-win\",\"acc-win\",\"bogus-win\",\"preferences-win\"]', '[\"preferences-win\"]', 'f9f9f9', 'Ladybuggin', 'center', 'Dark Gray', 'true');
+INSERT INTO `desktop_themes` VALUES ('Vista-black', 'resources/themes/xtheme-vista-black/xtheme-vista.png', 'resources/themes/xtheme-vista-black/vista-black.css');
+INSERT INTO `desktop_themes` VALUES ('Vista-clear', 'resources/themes/xtheme-vista-clear/xtheme-vista.png', 'resources/themes/xtheme-vista-clear/vista-clear.css');
+INSERT INTO `desktop_themes` VALUES ('Windows-blue', 'resources/themes/xtheme-windows-blue/xtheme-vista.png', 'resources/themes/xtheme-windows-blue/windows-blue.css');
 -- --------------------------------------------------------
 
 --
@@ -139,8 +142,8 @@ INSERT INTO `desktop_wallpapers` VALUES ('Beauty1', 'resources/wallpapers/thumbn
 INSERT INTO `desktop_wallpapers` VALUES ('Beauty2', 'resources/wallpapers/thumbnails/beauty2.jpg', 'resources/wallpapers/beauty2.jpg');
 INSERT INTO `desktop_wallpapers` VALUES ('Beauty3', 'resources/wallpapers/thumbnails/beauty3.jpg', 'resources/wallpapers/beauty3.jpg');
 INSERT INTO `desktop_wallpapers` VALUES ('Beauty4', 'resources/wallpapers/thumbnails/beauty4.jpg', 'resources/wallpapers/beauty4.jpg');
-INSERT INTO `desktop_wallpapers` VALUES ('Beauty4', 'resources/wallpapers/thumbnails/beauty5.jpg', 'resources/wallpapers/beauty5.jpg');
-INSERT INTO `desktop_wallpapers` VALUES ('Beauty4', 'resources/wallpapers/thumbnails/beauty6.jpg', 'resources/wallpapers/beauty6.jpg');
-INSERT INTO `desktop_wallpapers` VALUES ('Beauty4', 'resources/wallpapers/thumbnails/beauty7.jpg', 'resources/wallpapers/beauty7.jpg');
-INSERT INTO `desktop_wallpapers` VALUES ('Beauty4', 'resources/wallpapers/thumbnails/beauty8.jpg', 'resources/wallpapers/beauty8.jpg');
-INSERT INTO `desktop_wallpapers` VALUES ('Beauty4', 'resources/wallpapers/thumbnails/beauty9.jpg', 'resources/wallpapers/beauty9.jpg');
+INSERT INTO `desktop_wallpapers` VALUES ('Beauty5', 'resources/wallpapers/thumbnails/beauty5.jpg', 'resources/wallpapers/beauty5.jpg');
+INSERT INTO `desktop_wallpapers` VALUES ('Beauty6', 'resources/wallpapers/thumbnails/beauty6.jpg', 'resources/wallpapers/beauty6.jpg');
+INSERT INTO `desktop_wallpapers` VALUES ('Beauty7', 'resources/wallpapers/thumbnails/beauty7.jpg', 'resources/wallpapers/beauty7.jpg');
+INSERT INTO `desktop_wallpapers` VALUES ('Beauty8', 'resources/wallpapers/thumbnails/beauty8.jpg', 'resources/wallpapers/beauty8.jpg');
+INSERT INTO `desktop_wallpapers` VALUES ('Beauty9', 'resources/wallpapers/thumbnails/beauty9.jpg', 'resources/wallpapers/beauty9.jpg');
